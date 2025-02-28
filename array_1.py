0b2788f514fd90111ac48a5390e913a354884d4f
shariqsajad88 <bhatshariq88@gmail.com>
2025-02-28 15:05:55 +0530

    added new file arrai.py

---
+++ array_1.py
@@ -0,0 +1,14 @@
+arr = [2,3,4,5,6.2,3.5, 3.6]
+
+print(arr)
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[3])
+print(arr[4])
+print(arr[5])
+
+arr.append(10)
+print(arr)
+arr.insert(2,10)
+print(arr) 
